8c47178fddb21451287f8eb5726810dcaac158ff
Ramazan Chasygov <theliketurbo@gmail.com>
2021-09-15 18:08:40 +0300

    feat: implement func for returning duration

--- src/utils/getFormattedDate.js
+++ src/utils/getFormattedDate.js
@@ -2,9 +2,10 @@ import dayjs from "dayjs";
 
 /**
  * Return date in form of "15 September 2021 17:09"
+ * 
  * @param {number} timestamp
  * @returns {string}
  */
-export default function formatDate(timestamp) {
+export default function getFormattedDate(timestamp) {
 	dayjs(timestamp).format("DD MMMM YYYY HH:MM")
 }
