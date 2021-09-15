cf1a40d62b9162762715e29e26c415e27d1e9d18
Ramazan Chasygov <theliketurbo@gmail.com>
2021-09-15 17:37:42 +0300

    feat: implement date format func

---
+++ src/utils/formatDate.js
@@ -0,0 +1,10 @@
+import dayjs from "dayjs";
+
+/**
+ * Return date in form of "15 September 2021 17:09"
+ * @param {number} timestamp
+ * @returns {string}
+ */
+export default function formatDate(timestamp) {
+	dayjs(timestamp).format("DD MMMM YYYY HH:MM")
+}
