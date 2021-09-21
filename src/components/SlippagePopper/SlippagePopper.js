7a61f10922e1066f667cdd270920b1615288dad3
Ramazan Chasygov <theliketurbo@gmail.com>
2021-09-21 11:45:46 +0300

    feat: move slippage into mini modal

---
+++ src/components/SlippagePopper/SlippagePopper.js
@@ -0,0 +1,45 @@
+import Popper from '@mui/material/Popper';
+import Paper from '@mui/material/Paper';
+import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+import Grow from "@mui/material/Grow";
+import PercentageTextField from '../../components/PercentageTextField/PercentageTextField';
+import classNames from "./SlippagePopper.module.scss";
+
+export default function SlippagePopper({ slippageState, popperState }) {
+	const {
+		slippage,
+		setSlippage,
+	} = slippageState;
+
+	const {
+		id,
+		open,
+		anchorEl,
+	} = popperState;
+
+	function handleSetSlippage(e) {
+		const newValue = Number(e.target.value.replace("%", ""));
+		setSlippage(newValue);
+	}
+
+	return (
+		<Popper id={id} open={open} anchorEl={anchorEl} placement="bottom-start" transition>
+			{({ TransitionProps }) => (
+				<Grow {...TransitionProps} style={{ transformOrigin: "top right" }} timeout={350}>
+					<Paper variant="outlined" classes={{ root: classNames.container }}>
+						<Stack spacing={2} direction={"row"} sx={{ alignItems: "center" }}>
+							<Stack spacing={1}>
+								<Typography>Slippage tolerance:</Typography>
+								<PercentageTextField placeholder="0.10%" value={slippage} onChange={handleSetSlippage} sx={{ maxWidth: "165px", maxHeight: "45px" }} />
+							</Stack>
+							<Box sx={{ maxWidth: "256px" }}>Your transaction will revert if the price changes
+								unfavorably by more than this percentage</Box>
+						</Stack>
+					</Paper>
+				</Grow>
+			)}
+		</Popper>
+	);
+}
